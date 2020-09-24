76b6a00aaf1044bd1e922e0c0d2225dc7da20279
Noncho Savov <inthewoodsbg@yahoo.com>
2020-09-25 01:49:35 +0300

    Update PlayerLaser.js
    First update - will fix most of the issues in the initial submission.

--- src/scripts/classes/PlayerLaser.js
+++ src/scripts/classes/PlayerLaser.js
@@ -18,8 +18,8 @@ class PlayerLaser extends Missile {
 		ctx.rotate(Math.radians(Math.random() * 360));
 		ctx.beginPath();
 		ctx.globalAlpha = 0.5 + Math.random()/2;
-		ctx.rect(-this.size*Math.random()*3, -this.size*2*Math.random()*4, this.size*3+this.size*Math.random(), this.size*3+this.size*Math.random());
 		ctx.fillStyle = this.lightColor;
+		ctx.rect(-this.size*Math.random()*3, -this.size*2*Math.random()*4, this.size*3+this.size*Math.random(), this.size*3+this.size*Math.random());
 		ctx.fill();
 		ctx.closePath();
 
@@ -33,19 +33,8 @@ class PlayerLaser extends Missile {
 		effectsCtx.globalAlpha = 0.2 + Math.random() / 4;
 		effectsCtx.beginPath();
 		effectsCtx.fillStyle = this.lightColor;
-		if (!this.angle) { // |
-			effectsCtx.rect(this.x/5-Math.random()*pwr/2, this.y/5-Math.random()*5 - (this.pulse==-1 ? 0 : 6), pwr/2, (this.pulse==-1 ? 9 : 3) + Math.random()*5);
-			effectsCtx.rect(this.x/5-Math.random()*pwr/4, this.y/5-Math.random()*5 - (this.pulse==-1 ? 0 : 6), pwr/4, (this.pulse==-1 ? 9 : 3) + Math.random()*5);
-		} else if ((this.angle==-30 && this.type!=8) || (this.angle==30 && this.type==8)) {
-			effectsCtx.rect(this.x/5-(this.pulse==-1?6:3)+Math.random()*3, this.y/5-(this.pulse==-1?6:3)+Math.random()*3, 1+Math.random()*3, 1+Math.random()*3);
-			effectsCtx.rect(this.x/5+(this.pulse==-1?6:3)+Math.random()*3, this.y/5+(this.pulse==-1?6:3)+Math.random()*3, 1+Math.random()*3, 1+Math.random()*3);
-		} else if (this.angle==30 || (this.angle==-30 && this.type==8)) {
-			effectsCtx.rect(this.x/5+(this.pulse==-1?6:3)+Math.random()*3, this.y/5-(this.pulse==-1?6:3)+Math.random()*3, 1+Math.random()*3, 1+Math.random()*3);
-			effectsCtx.rect(this.x/5-(this.pulse==-1?6:3)+Math.random()*3, this.y/5+(this.pulse==-1?6:3)+Math.random()*3, 1+Math.random()*3, 1+Math.random()*3);
-		} else { // ---
-			effectsCtx.rect(this.x/5-Math.random()*5 - (this.pulse==-1 ? 0 : 6), this.y/5-Math.random()*pwr/2, (this.pulse==-1 ? 9 : 3) + Math.random()*5, pwr/2);
-			effectsCtx.rect(this.x/5-Math.random()*5 - (this.pulse==-1 ? 0 : 6), this.y/5-Math.random()*pwr/4, (this.pulse==-1 ? 9 : 3) + Math.random()*5, pwr/4);
-		}
+		effectsCtx.rect(this.x/5-Math.random()*pwr/2, this.y/5-Math.random()*5 - (this.pulse==-1 ? 0 : 6), pwr/2, (this.pulse==-1 ? 9 : 3) + Math.random()*5);
+		effectsCtx.rect(this.x/5-Math.random()*pwr/4, this.y/5-Math.random()*5 - (this.pulse==-1 ? 0 : 6), pwr/4, (this.pulse==-1 ? 9 : 3) + Math.random()*5);
 		
 		effectsCtx.fill();
 		effectsCtx.closePath();
